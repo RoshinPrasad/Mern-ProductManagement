e41e39cabe0fef9d05ae0c7d0827d363be91af44
Roshin Prasad <88421254+RoshinPrasad@users.noreply.github.com>
2024-02-11 09:21:27 +0530

    Update api.jsx

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://swaroin.online/api',
+  baseURL: 'https://clientapi-roan.vercel.app/api',
   // baseURL: 'http://localhost:5000/api',
 });
 
