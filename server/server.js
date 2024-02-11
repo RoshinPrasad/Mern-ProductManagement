e509541e607f1a666cf3cd52dcd994d8282afaa9
unknown <roshinandalt275@gmail.com>
2024-02-11 06:19:30 +0530

    fourth commit

--- server/server.js
+++ server/server.js
@@ -13,14 +13,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect('mongodb+srv://product:product@productmanagement.ycanggd.mongodb.net/productmanagement');
 
 
-const corsOptions = {
-  origin: 'https://clientappyo.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-
-};
-
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
