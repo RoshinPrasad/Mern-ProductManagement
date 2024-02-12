1df1c84e853f0b49cbf67ab8be022045270928b0
Roshin Prasad <88421254+RoshinPrasad@users.noreply.github.com>
2024-02-12 16:53:30 +0530

    Update api.jsx

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://clientapi-roan.vercel.app/api',
+  baseURL: 'https://mern-products.onrender.com/api',
   // baseURL: 'http://localhost:5000/api',
 });
 
